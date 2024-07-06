aa931be81776714ad5e4e9ba310d2ba158be3860
Romzorus <romzorus591@gmail.com>
2024-07-07 00:11:28 +0200

    Initial commit

---
+++ src/error/mod.rs
@@ -0,0 +1,15 @@
+// Definition of all possible errors for the whole crate
+// FIXME : relevant to subdivide this into multiple enums ?
+
+#[derive(Debug)]
+pub enum Error {
+    FailureToFindGroupContent,
+    FailureToParseContent(String),
+    FailureToRunCommand(String),
+    FailedInitialization(String),
+    FailedTcpBinding(String),
+    FailedTaskDryRun(String),
+    MissingInitialization,
+    GroupNotFound,
+    MissingGroupsList,
+}
